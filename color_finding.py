d99ddb54c7e0f2f0a2d86c3bb9c905686c07bb11
Celine Ta <celine.ta@students.olin.edu>
2015-10-17 17:17:42 -0400

    use only red mask, save to image

--- color_finding.py
+++ color_finding.py
@@ -8,10 +8,10 @@ import cv2
 image = cv2.imread('test.png')
 # define the list of boundaries
 boundaries = [
-	([17, 15, 100], [50, 56, 200]),
-	([86, 31, 4], [220, 88, 50]),
-	([25, 146, 190], [62, 174, 250]),
-	([103, 86, 65], [145, 133, 128])
+	([17, 15, 100], [50, 56, 200])#, red in BGR
+	#([86, 31, 4], [220, 88, 50]),
+	#([25, 146, 190], [62, 174, 250]),
+	#([103, 86, 65], [145, 133, 128])
 ]
 
 # loop over the boundaries
@@ -29,3 +29,5 @@ for (lower, upper) in boundaries:
 	# show the images
 	cv2.imshow("images", np.hstack([image, output]))
 	cv2.waitKey(0)
+
+	cv2.imwrite("images.png", np.hstack([image,output]))
