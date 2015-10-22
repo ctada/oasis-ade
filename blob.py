2af4cf2ff93625a47d5148f6e492fe1dbdabdb0d
pdemetci <pinardemetci@gmail.com>
2015-10-21 21:53:08 -0400

    Playing with threshold

--- blob.py
+++ blob.py
@@ -3,17 +3,27 @@ import cv2
 import numpy as np;
  
 # Read image
-im = cv2.imread("4tube720x720.jpg", cv2.IMREAD_GRAYSCALE)
+im = cv2.imread("4tube720x720.jpg")
  
 # Set up the detector with default parameters.
 params = cv2.SimpleBlobDetector_Params()
+
+params.filterByArea = True
+params.minArea = 20
+params.maxThreshold = 150
+params.minThreshold = 1
+params.filterByInertia = True
+params.minInertiaRatio = 0.0000001
+
+print params.filterByCircularity, params.minCircularity
+
+
 detector = cv2.SimpleBlobDetector_create(params)
  
 # Detect blobs.
 keypoints = detector.detect(im)
 count = 0
 for i in keypoints:
-	
 	x = keypoints[count].pt[0] #i is the index of the blob you want to get the position
 	y = keypoints[count].pt[1]
 	count+=1
